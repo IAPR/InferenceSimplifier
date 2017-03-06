f8d12ea1348ea2490e643fcd46f71453705f82dc
kinokoio <kinokoio@hotmail.com>
2017-03-06 08:48:35 -0600

    Added read only text edit

--- gui.py
+++ gui.py
@@ -18,6 +18,9 @@ class InferenceSolver(QWidget):
 
         self.refresh_button = QPushButton("Evaluate!")
 
+        self.results = QPlainTextEdit()
+        self.results.setReadOnly(True)
+
         self.hlayout = QHBoxLayout()
         self.hlayout.addWidget(self.antecedents)
         self.hlayout.addWidget(self.mid_lbl)
@@ -27,6 +30,7 @@ class InferenceSolver(QWidget):
         self.main_layout = QGridLayout()
         self.main_layout.addWidget(self.inst_lbl, 0, 0)
         self.main_layout.addLayout(self.hlayout, 1, 0)
+        self.main_layout.addWidget(self.results, 2, 0)
 
         self.refresh_button.clicked.connect(self.Evaluate)
 
